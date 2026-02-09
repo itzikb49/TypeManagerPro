d53b6d75eb76c7c2b95d1f789fc9b1dd9e68fa52
Itzik <itzikb49@gmail.com>
2026-02-09 22:06:18 +0200

    Current state with sync issues

--- Helpers/RenameHelper.cs
+++ Helpers/RenameHelper.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
+// using System.Text.RegularExpressions;
 using Autodesk.Revit.DB;
 
 namespace TypeManagerPro.Helpers
